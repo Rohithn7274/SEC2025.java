6cb62050891c8b6ff53f74bae983c5337db138a1
Rohithn7274 <eng24am0265@dsu.edu.in>
2025-09-16 15:24:39 +0530

    Add files via upload

---
+++ TernaryExample.java
@@ -0,0 +1,7 @@
+public class TernaryExample{
+    public static void main(String[] args){
+        int num=10;
+        String result=(num%2==0)?"Even":"Odd";
+        System.out.println("The number "+num+" is "+result);
+    }
+}
